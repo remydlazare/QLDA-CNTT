b05fba63b7ff8c4be18ae662846699aa694f12f3
lvtk12HaUI <cuncondangyeu28041999@gmail.com>
2020-12-26 11:29:46 +0700

    Add demo product

--- resources/views/user/blocks/slider.blade.php
+++ resources/views/user/blocks/slider.blade.php
@@ -3,16 +3,7 @@
     <div class="flexslider" id="mainslider">
       <ul class="slides">
         <li>
-          <img src="{!! url('user/img/banner12.jpg') !!}" alt="" />
-        </li>
-        <li>
-          <img src="{!! url('user/img/banner11.jpg') !!}" alt="" />
-        </li>
-        <li>
-          <img src="{!! url('user/img/banner3.jpg') !!}" alt="" />
-        </li>
-        <li>
-          <img src="{!! url('user/img/banner3.jpg') !!}" alt="" />
+          <img src="{!! url('user/img/banner456.jpg') !!}" alt="" />
         </li>
       </ul>
     </div>
